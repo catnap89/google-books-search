31abf9f99bfb12186711c1b5d67d0d25f0841500
youngwoo.cho <petshop0907@gmail.com>
2019-10-22 03:24:55 -0400

    required mongoose, express and routes folder and set up the ports for node express server and mongodb

---
+++ server.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const routes = require("./routes");
+
+// process.env.PORT is used on Heroku to dynamically set the port number
+const PORT = process.env.PORT || 3000;
+
+const app = express();  // Express App - Server
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
