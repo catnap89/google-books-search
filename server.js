3be2d389f5d3ef71f8a9162df92079a8f6539f34
youngwoo.cho <petshop0907@gmail.com>
2019-10-25 18:18:59 -0400

    changed mongodb connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://catnap89:Dontnap8(@ds139896.mlab.com:39896/heroku_zj578kwm");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Start the API server
 app.listen(PORT, function() {
